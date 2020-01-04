14aef9ec32e51fdd1f591cf6ad1a24afda992fc9
Simas2006 <sjuknelis@gmail.com>
2020-01-04 16:02:17 -0500

    Implement music selection page + general reset fixes

--- static/script.js
+++ static/script.js
@@ -1,19 +1,16 @@
 var fs = require("fs");
 var alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+var validMusicExts = [".mp3",".m4a",".wav",".ogg"];
 var mplayer;
 
 class MusicPlayer {
   constructor() {
     this.audioObject = document.getElementById("audioObject");
     this.audioObject.onended = _ => this.playNextSong();
-    this.active = true;
-    this.currentlyPlaying = true;
-    this.albumName = "Folder B";
-    this.albumSongs = [
-      "1-03 My Shot.m4a",
-      "2-02 Cabinet Battle #1.m4a",
-      "2-19 The Election of 1800.m4a"
-    ];
+    this.active = false;
+    this.currentlyPlaying = false;
+    this.albumName = null;
+    this.albumSongs = [];
     this.currentSongIndex = -1;
     this.volume = 100;
     this.savedVolume = -1;
@@ -29,7 +26,7 @@ class MusicPlayer {
       return;
     }
     this.audioObject.src = `${__dirname}/../data/music/${encodeURIComponent(this.albumName)}/${encodeURIComponent(this.albumSongs[this.currentSongIndex])}`;
-    this.togglePlay(true);
+    this.togglePlay(true,true);
     var songName = this.albumSongs[this.currentSongIndex];
     for ( var i = 0; i < songName.length; i++ ) {
       if ( alphabet.includes(songName.charAt(0)) ) break;
@@ -40,6 +37,7 @@ class MusicPlayer {
     document.getElementById("home-song-name").innerText = `${songName} (${this.currentSongIndex + 1} of ${this.albumSongs.length})`;
   }
   resetAll() {
+    this.togglePlay(true,false);
     this.active = false;
     this.albumName = null;
     this.albumSongs = [];
@@ -56,9 +54,10 @@ class MusicPlayer {
     if ( this.volume > 0 ) this.savedVolume = -1;
     document.getElementById("home-volume-button").innerText = `${this.volume}%`;
   }
-  togglePlay(setState) {
+  togglePlay(setState,value) {
+    if ( ! this.active ) return;
     var nextState = ! this.currentlyPlaying;
-    if ( setState ) nextState = true;
+    if ( setState ) nextState = value;
     if ( nextState ) {
       document.getElementById("home-play-button").innerText = "||";
       this.audioObject.play();
@@ -70,10 +69,11 @@ class MusicPlayer {
   }
   updateTime() {
     var pad = n => n < 10 ? "0" + n : n.toString();
-    var currentTime = this.audioObject.currentTime || 0;
+    var currentTime = Math.floor(this.audioObject.currentTime || 0);
     var duration = this.audioObject.duration || 0;
-    document.getElementById("home-start-time").innerText = pad(Math.floor(currentTime / 60)) + ":" + pad(Math.floor(currentTime % 60));
-    document.getElementById("home-end-time").innerText = pad(Math.floor(duration / 60)) + ":" + pad(Math.floor(duration % 60));
+    var timeLeft = Math.floor(duration - currentTime);
+    document.getElementById("home-start-time").innerText = pad(Math.floor(currentTime / 60)) + ":" + pad(currentTime % 60);
+    document.getElementById("home-end-time").innerText = "-" + pad(Math.floor(timeLeft / 60)) + ":" + pad(timeLeft % 60);
     document.getElementById("home-current-progress").style.width = `${(currentTime / duration) * 15}em`
   }
   muteButton() {
@@ -92,6 +92,43 @@ class MusicPlayer {
   }
 }
 
+function renderMusicPage() {
+  fs.readdir(`${__dirname}/../data/music`,function(err,list) {
+    if ( err ) throw err;
+    list = list.filter(item => ! item.startsWith("."));
+    var albumObj = document.getElementById("music-album-list");
+    while ( albumObj.firstChild ) {
+      albumObj.removeChild(albumObj.firstChild);
+    }
+    for ( var i = 0; i < list.length; i++ ) {
+      var button = document.createElement("button");
+      button.innerText = list[i];
+      button["data-album"] = list[i];
+      button.onclick = function() {
+        var album = this["data-album"];
+        fs.readdir(`${__dirname}/../data/music/${album}`,function(err,list) {
+          list = list.filter(item => validMusicExts.map(jtem => item.endsWith(jtem)).includes(true));
+          mplayer.albumName = album;
+          mplayer.albumSongs = list;
+          mplayer.currentSongIndex = -1;
+          mplayer.active = true;
+          openPage("home");
+          mplayer.playNextSong();
+        });
+      }
+      albumObj.appendChild(button);
+    }
+  });
+}
+
+function openPage(page) {
+  Array.from(document.getElementsByClassName("page")).forEach(function(item) {
+    item.style.display = item.id == page + "-page" ? "inline" : "none";
+  });
+  if ( page == "music" ) renderMusicPage();
+}
+
 window.onload = function() {
+  openPage("home");
   mplayer = new MusicPlayer();
 }
